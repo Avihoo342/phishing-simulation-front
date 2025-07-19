d74f8d3326d8b585e97477fcc85463e6b6e02723
avihooCohenNice <Avihoo.Cohen@nice.com>
2025-07-19 20:59:13 +0300

    CYMULATE- upload-front

--- src/pages/PhishingSimulationPage/PhishingSimulationPage.tsx
+++ src/pages/PhishingSimulationPage/PhishingSimulationPage.tsx
@@ -23,7 +23,6 @@ export default function PhishingSimulationPage() {
 
   const handleSend = async (e: React.FormEvent) => {
     e.preventDefault();
-    console.log(email)
     await simulationAPI.post('/phishing/send', { to: email });
     fetchAttempts();
   };
